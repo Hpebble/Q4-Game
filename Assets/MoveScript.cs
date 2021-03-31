6a76b3a838558a49814ce991acdc1aae296a3448
Kendrick Notarte <momokoi321@gmail.com>
2021-03-31 16:47:08 -0700

    Importing stuff

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed;
+    float x;
+    float z;
+    public Rigidbody rb;
+    public Transform DirectionalLight;
+    public float rotateSpeed;
+    public float goingRight;
+    public float goingLeft;
+    public float dlxx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+
+        dlxx += rotateSpeed;
+        DirectionalLight.rotation = Quaternion.Euler(dlxx, DirectionalLight.rotation.y, DirectionalLight.rotation.x);
+        Debug.Log(DirectionalLight.rotation);
+        
+        
+        if (x > 0)
+        {
+           
+          
+        }
+
+        if (x == -1)
+        { 
+                       
+        }
+
+        rb.velocity = new Vector3(x * speed, rb.velocity.y, z * speed);
+    }
+}
