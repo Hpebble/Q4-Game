13e373472a86c385af4cceb2d926e61a42f8445e
circumvention21 <Fleshh13>
2021-05-26 14:40:11 -0700

    castle level

---
+++ Assets/keyPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyPickup : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject Knight;
+    public GameObject Door;
+    public ParticleSystem keyParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key.SetActive(true);
+        Door.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Player has touched the key");
+            key.SetActive(false);
+            Door.SetActive(false);
+            keyParticle.Play();
+        }
+    }
+}
